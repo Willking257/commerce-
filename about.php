e106a3fff49301b17af82203339bd1fd6009b1e8
morpeau pierre stanley <morpeau pierre stanley>
2024-09-12 15:34:06 -0400

    this the commit for the page about

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/index.css?v=16" />
+    <?php include('asset/include/cdn.php') ?>
+    <?php include('asset/include/script-cdn.php') ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="about">
+            <section class="header-div b-center d-block " style="background-image: url(https://charitfix.vercel.app/theme/assets/images/gallery/breadcrumb-1.png); height:350px; ">
+                <span class="">
+                    <p class="fs-3 ms-5"> <a class="text-decoration-none text-white  border border-white p-2 rounded-2" href="">HOME > ABOUT US</a></p>
+                </span>
+                <span class=" ms-5 fs-1 text-white">
+                    ABOUT
+                </span>
+            </section>
+        </div>
+
+        <!-- deuxieme div -->
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col-md-3 mb-4">
+                    <div class="div-about border_third border-span rounded-2 box-shadow-third">
+                        <div class="icon-1 b-center d-center mt-4 rounded-3">
+                            <i class='bx bxs-hand'></i>
+                        </div>
+                        <span class="fs-2 d-center pt-3">
+                            <strong>Medical & Blood</strong>
+                        </span>
+                        <span class=" d-center">
+                            <span class="fs-3 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
+                        </span>
+
+                        <div class="btn_third d-center py-5">
+                            <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
+                                <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
+                                <strong class="fs-3 text-success">READ MORE</strong>
+                                    <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-3"></div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
