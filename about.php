9f9f253e5f0166f7b29dcd31e774c27390b303f3
Smith Brinvil <brinvilsmith@gmail.com>
2024-09-13 14:26:04 -0400

    this is the push for the about page

--- about.php
+++ about.php
@@ -26,33 +26,150 @@
 
         <!-- deuxieme div -->
         <div class="container">
-            <div class="row pt-3">
-                <div class="col-md-3 mb-4">
-                    <div class="div-about border_third border-span rounded-2 box-shadow-third">
-                        <div class="icon-1 b-center d-center mt-4 rounded-3">
-                            <i class='bx bxs-hand'></i>
-                        </div>
-                        <span class="fs-2 d-center pt-3">
-                            <strong>Medical & Blood</strong>
-                        </span>
-                        <span class=" d-center">
-                            <span class="fs-3 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
-                        </span>
+            <div class="about pt-5">
+                <div class="row pt-3">
+                    <div class="col-md-3 mb-4  px-2">
+                        <div class="div-about border_third border-span rounded-2 box-shadow-third  py-2">
+                            <div class="icon-1 b-center d-center mt-4 rounded-3">
+                                <i class='bx bxs-hand'></i>
+                            </div>
+                            <span class="fs-2 d-center pt-2">
+                                <strong class="fs-4">Medical & Blood</strong>
+                            </span>
+                            <span class=" d-center">
+                                <span class="fs-4 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
+                            </span>
 
-                        <div class="btn_third d-center py-5">
-                            <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
-                                <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
-                                <strong class="fs-3 text-success">READ MORE</strong>
-                                    <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
-                                </a>
+                            <div class="btn_third d-center py-2">
+                                <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
+                                    <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
+                                        <strong class="fs-6 text-success">READ MORE</strong>
+                                        <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-4  px-2">
+                        <div class="div-about border_third border-span rounded-2 box-shadow-third  py-2">
+                            <div class="icon-1 b-center d-center mt-4 rounded-3">
+                                <i class='bx bxs-hand'></i>
+                            </div>
+                            <span class="fs-2 d-center pt-2">
+                                <strong class="fs-4">Medical & Blood</strong>
+                            </span>
+                            <span class=" d-center">
+                                <span class="fs-4 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
+                            </span>
+
+                            <div class="btn_third d-center py-2">
+                                <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
+                                    <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
+                                        <strong class="fs-6 text-success">READ MORE</strong>
+                                        <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-4  px-2">
+                        <div class="div-about border_third border-span rounded-2 box-shadow-third py-2">
+                            <div class="icon-1 b-center d-center mt-4 rounded-3">
+                                <i class='bx bxs-hand'></i>
+                            </div>
+                            <span class="fs-2 d-center pt-2">
+                                <strong class="fs-4">Medical & Blood</strong>
+                            </span>
+                            <span class=" d-center">
+                                <span class="fs-4 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
+                            </span>
+
+                            <div class="btn_third d-center py-2">
+                                <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
+                                    <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
+                                        <strong class="fs-6 text-success">READ MORE</strong>
+                                        <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-4  px-2">
+                        <div class="div-about border_third border-span rounded-2 box-shadow-third py-2">
+                            <div class="icon-1 b-center d-center mt-4 rounded-3">
+                                <i class='bx bxs-hand'></i>
+                            </div>
+                            <span class="fs-2 d-center pt-2">
+                                <strong class="fs-4">Medical & Blood</strong>
+                            </span>
+                            <span class=" d-center">
+                                <span class="fs-4 text-muted text-center">When deciding which charity to donate to, it important to do your .</span>
+                            </span>
+
+                            <div class="btn_third d-center py-2">
+                                <div class="d-flex justify-content-center align-items-center "> <!-- Centre le contenu horizontalement et verticalement -->
+                                    <a class="d-flex align-items-center fs-6 text-decoration-none btn-custom pt-3" href="">
+                                        <strong class="fs-6 text-success">READ MORE</strong>
+                                        <span class="ms-2"><i class='bx bx-right-top-arrow-circle'></i></span>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+
+            <div class="about-us pt-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <span class="d-block text-success fs-2">About us</span>
+                            <strong class="fs-2 d-block mb-3">We Are Best Treatment For Our Hospita</strong>
+                            <span class="fs-6 text-muted d-block">When deciding which charity to donate to, it's important to do your e search and find one that aligns with your values and interests. Look for charities that are transparent preventable diseases,</span>
+
+                            <br>
+                            <span class="fs-6 text-muted d-block">When deciding which charity to donate to, it's important to do your e search and find one that aligns with your values and interests. Look for charities that are transparent preventable diseases,</span>
+                            <div class="d-flex gap-3 mt-4">
+                                <span class="fs-3"><i class='bx bx-donate-heart'></i></span>
+                                <span>
+                                    <span class="fs-4 text-black d-block">Donation</span>
+                                    <span class="fs-6 text-muted d-block">Fund programs that help children escape poverty providing vocational training.</span>
+                                </span>
+
+                            </div>
+
+                            <div class="d-flex gap-3 mt-2">
+                                <span class="fs-3"><i class='bx bx-user'></i></span>
+                                <span>
+                                    <span class="fs-4 text-black d-block">Volunteer</span>
+                                    <span class="fs-6 text-muted d-block">Fund programs that help children escape poverty providing vocational training.</span>
+
+                                </span>
+
+                            </div>
+                            <span class="m-5 d-block"><a class="btn-green text-decoration-none rounded-2 px-4 fs-5 pt-3" href="">Read More</a></span>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="image b-center mt-5">
+                                <span>
+                                    <img class="img-fluid h-100 w-100 pt-3" src="https://charitfix.vercel.app/theme/assets/images/hero/hero-1.png" alt="" />
+                                </span>
                             </div>
                         </div>
-
                     </div>
                 </div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
+
+
             </div>
         </div>
 
